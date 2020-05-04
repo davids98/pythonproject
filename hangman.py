506278cd83843c03461433246f64b9aa25a749a0
David Sam <davids98@vt.edu>
2020-05-04 00:43:18 -0400

    Sets structure of game

--- hangman.py
+++ hangman.py
@@ -1,1 +1,30 @@
-# This is my initial commit
+import random # import random lib
+from word_bank import words_to_choose # import the wordbank 
+
+def what_word_to_use():
+	target_word = random.choice(words_to_choose)
+	return target_word.lower()
+
+def game(target_word, level):
+	complete = "_"  * len(target_word)
+	correct = False;
+	letters_guess = []
+	words_guess = []
+	attempts_left = 6
+	if level == 0:
+	print("Hangman is about to begin! \n")
+	print("Level ")
+	print(str(level))
+	print("\n")
+	print(complete)
+	print("\n")
+	while not corrrect and attempts_left > 0:
+		attempt = input("Please guess a letter or word: " ).lower()
+		if len(attempt) == 1 and attempt.isalpha():
+
+		elif len(attempt) == len(word) and attempt.isalpha():
+
+		elif len(attempt) > 1 and attempt.isalpha():
+
+		else: 
+			print("Not a valid input. Please try again" )
--- word_bank.py
+++ word_bank.py
@@ -1,2 +1,5 @@
+# this file contains the words to pick from for the hang man game
+
+
 words_to_choose = [ "hello", "affable", "executive", "central", "conveninece", "assembly", "perforate", "clearance", "indefatigable", "nonplussed", "abject", "aberration", "abjure", "abrograte", "iconoclast", "gratuitous", "grandiloquent", "interlocutor", "invective", "intrasignet", "legerdemain", "multifarious", "munificent", "obdurate" 
 "obfuscate", "obstreperous", "peelucid" , "penurious" , "pertinacious" , " plaudit" , "quandry", "requisition" , "surreptitious", "vociferous", "zaitech", "zedoary" ]
