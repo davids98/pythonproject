bc6094c26458533fd11ddb8257220352b9af3bd4
David Sam <davids98@vt.edu>
2020-05-04 00:17:33 -0400

    Added words to wordfile

---
+++ word_bank.py
@@ -0,0 +1,2 @@
+words_to_choose = [ "hello", "affable", "executive", "central", "conveninece", "assembly", "perforate", "clearance", "indefatigable", "nonplussed", "abject", "aberration", "abjure", "abrograte", "iconoclast", "gratuitous", "grandiloquent", "interlocutor", "invective", "intrasignet", "legerdemain", "multifarious", "munificent", "obdurate" 
+"obfuscate", "obstreperous", "peelucid" , "penurious" , "pertinacious" , " plaudit" , "quandry", "requisition" , "surreptitious", "vociferous", "zaitech", "zedoary" ]
