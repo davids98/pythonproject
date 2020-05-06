c819bc0001f62f0872e3cd83e1fac8294ccb4f16
David Sam <davids98@vt.edu>
2020-05-06 16:28:45 -0400

    Adds commments to all files

--- hangman.py
+++ hangman.py
@@ -3,8 +3,8 @@ from word_bank import words_to_choose # import the wordbank
 from word_bank import hints_list
 
 
-def draw(attempt):
-	hanglist =[
+def draw(attempt):                              # defines the drawing for the hang man
+	hanglist =[ 
 		"""     \t\t\t-------------
 			|
 			|
@@ -83,11 +83,11 @@ def draw(attempt):
 	print(hanglist[6 - attempt])
 
 
-def what_word_to_use():
+def what_word_to_use():      # randomly selects a word to use
 	target_word = random.choice(words_to_choose)
 	return target_word.lower()
 
-def addToWord(word,letter,display):
+def addToWord(word,letter,display):  #adds letter guess to the word 
 	instance = []
 	for x in range(len(word)):
 		if letter == word[x]:
@@ -95,18 +95,18 @@ def addToWord(word,letter,display):
 	for j in range(len(instance)):
 		display[instance[j]] = word[instance[j]]			
 
-def game(target_word, level, hints):
-	complete = "_"  * len(target_word)
-	correct = False;
-	letters_guess = []
-	words_guess = []
-	attempts_left = 6
-	show_hint = False
+def game(target_word, level, hints):              # defines the logic for the game
+	complete = "_"  * len(target_word)           # sets the initial word to only dashes of the length of the word
+	correct = False;                             # boolean for correct word
+	letters_guess = []                             #empty array for letter guess
+	words_guess = [] 				# empty array for word guesses
+	attempts_left = 6				# tries left
+	show_hint = False				# determins whether to show hints
 	if level == 1:
 		print("\n H A N G M A N \n")
-		rules = input("Press Enter To Begin ...\nType in [ How ] for a HOW TO PLAY\n\n" ).lower()
+		rules = input("Press Enter To Begin ...\nType in [ How ] for a HOW TO PLAY\n\n" ).lower()  # requests user to read rules
 
-		if rules == "how":	
+		if rules == "how":	 # prints out how to play
 			print("\n You Are Lawyer Tasked With Defending 10 People Set To Be Hanged ")
 			print(" As You Are Making Your Case To The Hangman To Save These People You Suddenly Forget A Word On The Tip Of Your Tongue ")
 			print(" You Must Remember What The Word Was Before The Hangman Kills The People You Are Set To Save \n\n")
@@ -117,94 +117,92 @@ def game(target_word, level, hints):
 			print(" 5) Every Flawless Round Adds Another Hint For You To Use Whenever You Choose  \n")
 			input("Press Enter To Begin ..." )
 		
-	print("\n You Are On Level " , level)
+	print("\n You Are On Level " , level)  # states the level
 	print("\n")
 	draw(attempts_left)
 	while not correct and attempts_left > 0:
-		print(complete)
+		print(complete)   # prints out the letters guessed as of yet
 		if not show_hint:
-			print("Hints: ", hints)
+			print("Hints: ", hints)  # prints hints count
 		else:
-			num = words_to_choose.index(target_word)
-			print("Hint: ", hints_list[num] )
+			num = words_to_choose.index(target_word)   
+			print("Hint: ", hints_list[num] ) # prints actual hints
 		print("\n")
 		attempt = input("Please guess a letter or word: " ).lower()
-		if attempt == "hint" or attempt == " hint" or attempt == "[hint]" or attempt == "hint ":
+		if attempt == "hint" or attempt == " hint" or attempt == "[hint]" or attempt == "hint ":        # if word is a variation of hint
 			draw(attempts_left)
 			if ('hint' or '[hint]' or 'hint ' or ' hint') in words_guess:
-				print("\nYou have used the hint for this round. Keep Going" )
+				print("\nYou have used the hint for this round. Keep Going" )        # check if they already used hint
 			elif hints == 0:
-				print("\nYou have no more hints. Win a round flawlessly to gain another hint")
+				print("\nYou have no more hints. Win a round flawlessly to gain another hint") # check if hints are at 0
 			else:
 				show_hint = True
 				hints = hints -1
-				words_guess.append(attempt) 
+				words_guess.append(attempt)             # make show hint true and append hint to words typed in
 		elif len(attempt) == 1 and attempt.isalpha():
 			if attempt in letters_guess:
-				print("You've already guessed the letter: " ,attempt)
+				print("You've already guessed the letter: " ,attempt)   # let user know if they already yepd in letter
 			elif attempt not in target_word:
 				attempts_left -= 1
 				draw(attempts_left)
-				print("The letter [",attempt,"] is not in the word. Try again")
+				print("The letter [",attempt,"] is not in the word.") # let user know guess was incorrect
 				letters_guess.append(attempt)
 			else:
 				draw(attempts_left)
-				print("You guessed a correct letter!")
+				print("You guessed a correct letter!")    # let user know they were correct
 				letters_guess.append(attempt) 
 				convert_word = list(target_word)
 				display = list(complete)
-				addToWord(convert_word, attempt, display)
+				addToWord(convert_word, attempt, display) # add guessed leter to word
 				complete = ''.join(display)
-				if complete == target_word:
+				if complete == target_word:               # if total workd guess set corerect to True
 					correct = True	
 		elif len(attempt) == len(target_word) and attempt.isalpha():
 			if attempt in words_guess:
-				print("You've already guessed the word: " ,attempt)
+				print("You've already guessed the word: " ,attempt)  # let user know they already guessed word
 			elif attempt != target_word: 
 				attempts_left -= 1
 				draw(attempts_left)
-				print(attempt," is not the correct word. Try again")
+				print(attempt," is not the correct word.")   # let user know if word is in correct
 				words_guess.append(attempt)
 			else: 
 				correct = True
-				complete = target_word
+				complete = target_word                # if correct word set complete to True
 		elif len(attempt) > 1 and attempt.isalpha():
 			draw(attempts_left)
-			print("Guess does not have correct amount of letters. Try again." )
+			print("Guess does not have correct amount of letters. Try again." )      # let user know guess did not have correct amount of letters
 		else: 
 			draw(attempts_left)
-			print("Not a valid input. Please try again" )
+			print("Not a valid input. Please try again" )   # let user know guess was not valid
 
 	if correct:
-		print("You win this level!\n")
+		print("You win this level!\n")  # let user know they have won
 		if attempts_left == 6:
-			hints = hints + 1
+			hints = hints + 1   # check if flawless and add one to hint if so
 		return hints
 	else:
 		print("Sorry the guy you were supposed to save has been hanged. \nThe word was [" ,target_word ,"] \nThe HANGMAN has won. \nGame Over.")
-		return -1
+		return -1 # return -1 if game is over 
 
 
 def main():
-	use_word = what_word_to_use()
-	level = 1
+	use_word = what_word_to_use() # choose random word
+	level = 1       # initialize variables
 	hints = 1	
-	print(len(words_to_choose))
-	print(len(hints_list))
-	num_hints = game(use_word, level, hints)
-	while level < 10 and num_hints != -1:
-		play_on = input("Move on the next level? Yes/No: ")
+	num_hints = game(use_word, level, hints) # start game and return to number of hints
+	while level < 10 and num_hints != -1: # repeat until level 10 or uset runs out of attempts at a level
+		play_on = input("Move on the next level? Yes/No: ")  # ask user if they wan to continue playing
 		if play_on[0] == "y" or play_on[0] == "Y": 
-			use_word = what_word_to_use()
+			use_word = what_word_to_use()  
 			level = level + 1
 			num_hints = game(use_word, level,num_hints)
 		else:
 			break
 	if level == 10:
-		print("CONGRATULATIONS!!\nYou Beat The Hangman And Saved Everbody!!\nYou Win!!")
+		print("CONGRATULATIONS!!\nYou Beat The Hangman And Saved Everbody!!\nYou Win!!") # let user know they have one
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # main
 	main()
 
